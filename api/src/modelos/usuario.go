c9b1feb391ecd1254c353359c6208275f526e1cc
Julio Cesar de Almeida <CIANDT\jcalmeida@lnb021944bhz.ciandt.global>
2022-01-24 15:06:11 -0300

    Metodos Buscar Usuarios, Buscar Usuario e Atualizar Usuario

--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -8,17 +8,17 @@ import (
 
 // Usuario representa um usuário utilizando a rede social
 type Usuario struct {
-	ID       uint64    `json:"id, omitempty"`
-	Nome     string    `json:"nome, omitempty"`
-	Nick     string    `json:"nick, omitempty"`
-	Email    string    `json:"email, omitempty"`
-	Senha    string    `json:"senha, omitempty"`
-	criadoEm time.Time `json:"criadoEm, omitempty"`
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"criadoEm,omitempty"`
 }
 
 // Preparar vai chamar os métodos para validar e formatar o usuário recebido
-func (usuario *Usuario) Preparar() error {
-	if erro := usuario.validar(); erro != nil {
+func (usuario *Usuario) Preparar(etapa string) error {
+	if erro := usuario.validar(etapa); erro != nil {
 		return erro
 	}
 
@@ -26,7 +26,7 @@ func (usuario *Usuario) Preparar() error {
 	return nil
 }
 
-func (usuario *Usuario) validar() error {
+func (usuario *Usuario) validar(etapa string) error {
 	if usuario.Nome == "" {
 		return errors.New("O nome é obrigatório e não pode estar em branco")
 	}
@@ -39,7 +39,7 @@ func (usuario *Usuario) validar() error {
 		return errors.New("O Email é obrigatório e não pode estar em branco")
 	}
 
-	if usuario.Senha == "" {
+	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("A senha é obrigatória e não pode estar em branco")
 	}
 
