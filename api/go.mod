42a5865b105c379d4483e2c0221320f83abc84f6
Julio Cesar de Almeida <CIANDT\jcalmeida@lnb021944bhz.ciandt.global>
2022-01-24 16:20:15 -0300

    Deleting user, validating email, package Bcrypt

--- api/go.mod
+++ api/go.mod
@@ -8,3 +8,8 @@ require (
 )
 
 require github.com/go-sql-driver/mysql v1.6.0
+
+require (
+	github.com/badoux/checkmail v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
+)
--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -1,9 +1,12 @@
 package modelos
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuario representa um usuário utilizando a rede social
@@ -22,7 +25,9 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return erro
 	}
 
-	usuario.formatar()
+	if erro := usuario.formatar(etapa); erro != nil {
+		return erro
+	}
 	return nil
 }
 
@@ -38,6 +43,9 @@ func (usuario *Usuario) validar(etapa string) error {
 	if usuario.Email == "" {
 		return errors.New("O Email é obrigatório e não pode estar em branco")
 	}
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O email inserido é inválido")
+	}
 
 	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("A senha é obrigatória e não pode estar em branco")
@@ -46,8 +54,18 @@ func (usuario *Usuario) validar(etapa string) error {
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, erro := seguranca.Hash(usuario.Senha)
+		if erro != nil {
+			return erro
+		}
+
+		usuario.Senha = string(senhaComHash)
+	}
+	return nil
 }
