90e657a879e10bae4fb2bb3d7f5a1cb9f18d555c
Julio Cesar de Almeida <CIANDT\jcalmeida@lnb021944bhz.ciandt.global>
2022-01-26 15:35:38 -0300

    Busca de publicaçoes por id e por feed/seguindo

--- api/sql/dados.sql
+++ api/sql/dados.sql
@@ -1,11 +1,20 @@
 insert into usuarios (nome, nick, email, senha)
 values
-("Usuario 1", "usuario_1", "usuario1@gmal.com", "$2a$10$iBgjGBDqZdDET2oQBrJNreebLhqYYZxTrSMRaRlfplW7mmrko.ZDu"),
-("Usuario 2", "usuario_2", "usuario2@gmal.com", "$2a$10$iBgjGBDqZdDET2oQBrJNreebLhqYYZxTrSMRaRlfplW7mmrko.ZDu"),
-("Usuario 3", "usuario_3", "usuario3@gmal.com", "$2a$10$iBgjGBDqZdDET2oQBrJNreebLhqYYZxTrSMRaRlfplW7mmrko.ZDu");
+("Usuario 1", "usuario_1", "usuario1@gmal.com", "$2a$10$Iijgmi9BZczc6eJVPalGNelZn0kF2T2gaHzyga/7OAp4lKahAP6p2"),
+("Usuario 2", "usuario_2", "usuario2@gmal.com", "$2a$10$JsX1m1LyIbs6vNbwiJRf..ewCKdmrWA3ixn9MPOge8KXfMu2A5lr6"),
+("Usuario 3", "usuario_3", "usuario3@gmal.com", "$$2a$10$ZglAR.p/AMnj9izdYdDZk.9Jz.FBYpUdMQ9YCzvg.j6OfJfsfQ5eW"),
+("Usuario 4", "usuario_4", "usuario4@gmal.com", "senha": "$2a$10$MekJiw.MACuDP7S9t9TGoeQhAadFSl6lI5.78RammnUOgXjQyljoS");
 
 insert into seguidores (usuario_id, seguidor_id)
 values
 (1, 2),
 (3, 1),
 (1, 3);
+
+insert into publicacoes (titulo, conteudo, autor_id)
+values
+("Publicação do Usuário 1", "Essa é a publicação do usuário 1! Oba!", 1),
+("Publicação do Usuário 2", "Essa é a publicação do usuário 2! Oba!", 2),
+("Publicação do Usuário 3", "Essa é a publicação do usuário 3! Oba!", 3);
+
+
--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -9,6 +9,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // CriarPublicacao adiciona nova publicação no banco de dados
@@ -56,12 +59,53 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 // BuscarPublicacoes traz as publicações que apareceriam no feed do usuário
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.Buscar(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacoes)
 }
 
 // BuscarPublicacao traz uma única publicação
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacao)
 }
 
 // AtualizarPublicacao altera os dados de uma publicação
